ee48b1abd48b6d4ea76dd4bcb4e983b2c4626165
leocastroz <castroz.dev@gmail.com>
2023-06-23 12:37:40 -0300

    feat: Using "Tailwind CSS" style

--- src/components/PopupContainer.vue
+++ src/components/PopupContainer.vue
@@ -1,33 +1,10 @@
 <template>
-    <div class="description">
-        <h2 class="font-black uppercase text-sky-700 w-full">Bem-vindo à Popup</h2>
-        <p>
+    <div class="description mx-auto my-10 rounded-xl p-5 w-80 border-solid border-2 border-cyan-200">
+        <h2 class="font-black uppercase text-black w-full text-center py-3">Bem-vindo à Popup</h2>
+        <p class="text-center text-sm text-zinc-100">
             Esse é um componente de popup genérico,
             que pode ser usado para exibir qualquer
             conteúdo.
         </p>
     </div>
 </template>
-
-<style scoped>
-.description {
-    margin: 30px auto;
-    width: 300px;
-    border-radius: 10px;
-    text-align: center;
-    padding: 25px;
-    border: 2px solid #ffffff76;
-  }
-  
-  .description h2 {
-    font-size: 20px;
-    margin: 10px;
-  }
-  
-  .description p {
-    color: #ffffff6b;
-    font-size: 14px;
-    letter-spacing: 1px !important;
-  }
-  
-</style>
