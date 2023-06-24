cdd9842b87ba64f88ebc6281e211ae89caf078b6
leocastroz <castroz.dev@gmail.com>
2023-06-24 12:20:31 -0300

    refact: Organizing and removing dirty code

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,51 +1,51 @@
 <script>
-import "../assets/config/popup.css";
-import PopupHeader from "./PopupHeader.vue";
+import PopupHeaderModalTwo from "./PopupHeaderModalTwo.vue";
+import PopupHeaderModal from "./PopupHeaderModal.vue";
+import VideoSectionTwo from "./VideoSectionTwo.vue";
 import PopupContainer from "./PopupContainer.vue";
-import PopupHelp from "./PopupHelp.vue";
-import jsonData from "../data/popupConfig";
+import VideoSection from "./VideoSection.vue";
 import PopupButtons from "./PopupButtons.vue";
 import GenderSelect from "./GenderSelect.vue";
-import MyModal from "./MyModal.vue";
-import PopupHeaderModal from "./PopupHeaderModal.vue";
-import PopupHeaderModalTwo from "./PopupHeaderModalTwo.vue";
-import VideoSection from "./VideoSection.vue";
-import VideoSectionTwo from "./VideoSectionTwo.vue";
-import Button from "./Button.vue";
+import PopupHeader from "./PopupHeader.vue";
+import jsonData from "../data/popupConfig";
 import CheckInput from "./CheckInput.vue";
+import PopupHelp from "./PopupHelp.vue";
+import MyModal from "./MyModal.vue";
+import "../assets/config/popup.css";
+import Button from "./Button.vue";
 
 export default {
   components: {
-    PopupHeader,
-    PopupContainer,
-    PopupHelp,
-    PopupButtons,
-    GenderSelect,
-    MyModal,
-    PopupHeaderModal,
     PopupHeaderModalTwo,
-    VideoSection,
+    PopupHeaderModal,
     VideoSectionTwo,
-    Button,
-    CheckInput
+    PopupContainer,
+    PopupButtons,
+    VideoSection,
+    GenderSelect,
+    PopupHeader,
+    CheckInput,
+    PopupHelp,
+    MyModal,
+    Button
   },
   data() {
     return {
+      cupom: "https://github.com/leocastroz",
+      showSecondFormModal: true,
+      showSecondModal: false,
+      spinerLoading: false,
+      consentChecked: true,
+      modalSuccess: false,
+      showFormModal: true,
+      selectedGender: "",
+      modalCupom: false,
+      pastCode: false,
       openModals: [],
       formValues: [],
-      consentChecked: true,
+      cutCode: true,
       bases: false,
       jsonData,
-      showSecondModal: false,
-      selectedGender: "",
-      modalSuccess: false,
-      spinerLoading: false,
-      cupom: "https://github.com/leocastroz",
-      cutCode: true,
-      pastCode: false,
-      modalCupom: false,
-      showFormModal: true,
-      showSecondFormModal: true,
     };
   },
   props: {
@@ -135,170 +135,132 @@ export default {
 </script>
 
 <template>
-  <div v-if="config" class="popup">
-    <div class="mx-auto">
-      <PopupHeader />
-      <PopupContainer />
-      <PopupHelp />
-      <PopupButtons :openModal="openModal" />
-      <div v-if="modalSuccess"
-        class="modal left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
-        <MyModal :config="config" :spiner-loading="spinerLoading" :modal-cupom="modalCupom" :cupom="cupom"
-          :cut-code="cutCode" :past-code="pastCode" @closeNow="closeNow" @copiar-numero="copiarNumero"
-          @hidden-cut="hiddenCut" />
-      </div>
-      <transition name="modal-transition">
-        <div v-if="bases"
-          class="modal left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
-          <div class="after-modal max-w-sm absolute text-start rounded-xl">
-            <div class="py-2 px-5">
-              <PopupHeaderModal :config="config" @close-in="closeIn" v-if="true" />
-              <form @submit.prevent="submitForm">
-                <p class="font-bold text-lg py-3 text-white">
-                  {{ config.titleForms }}
-                </p>
-                <div v-for="(field, index) in config.formFields" :key="index"
-                  class="bg-black-100 flex justify-between items-center">
-                  <label :for="'field' + index" class="text-sm text-violet-300 pr-3">{{ field.label }}</label>
-                  <input :type="field.type" :id="'field' + index" :value="field.value"
-                    @input="updateFieldValue(index, $event.target.value)" required
-                    class="rounded border-none my-2 bg-violet-300 p-1 mr-10" />
-                </div>
-                <div class="flex items-center flex justify-between">
-                  <p class="text-violet-300 pr-2">{{ config.titleGender }}</p>
-                  <GenderSelect :options="config.gender" :selectedGender.sync="selectedGender" class="mr-36" />
-                </div>
-                <CheckInput
-                  v-if="config.consentCheckbox"
-                  inputId="consentCheckbox"
-                  :label="config.shareData"
-                />
-                <CheckInput
-                  v-if="config.consentCheckbox"
-                  inputId="consentCheckbox"
-                  :label="config.acceptTerms"
-                  :checked="consentChecked"
-                  @update:checked="consentChecked = $event"
-                />
-                <Button :is-form-valid="isFormValid" button-text="Enviar" />
-              </form>
-            </div>
-          </div>
-        </div>
-      </transition>
-      <transition name="modal-transition">
-        <div v-if="showFormModal && isModalOpen('form')"
-          class="modal left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
-          <div class="after-modal max-w-sm absolute text-start rounded-xl">
-            <div class="py-2 px-5">
-              <PopupHeaderModalTwo :config="config" @closeModal="closeModal" />
-              <form @submit.prevent="submitForm">
-                <p class="font-bold text-lg py-3 text-white">
-                  {{ config.titleForms }}
-                </p>
-                <div v-for="(field, index) in config.formFields" :key="index"
-                  class="bg-black-100 flex justify-between items-center">
-                  <label :for="'field' + index" class="text-sm text-violet-300 pr-3">{{ field.label }}</label>
-                  <input :type="field.type" :id="'field' + index" :value="field.value"
-                    @input="updateFieldValue(index, $event.target.value)" required
-                    class="rounded border-none my-2 bg-violet-300 p-1 mr-10" />
-                </div>
-                <div class="flex items-center flex justify-between">
-                  <p class="text-violet-300 pr-2">{{ config.titleGender }}</p>
-                  <GenderSelect :options="config.gender" :selectedGender.sync="selectedGender" class="mr-36" />
-                </div>
-                <CheckInput
-                  v-if="config.consentCheckbox"
-                  inputId="consentCheckbox"
-                  :label="config.shareData"
-                />
-                <CheckInput
-                  v-if="config.consentCheckbox"
-                  inputId="consentCheckbox"
-                  :label="config.acceptTerms"
-                  :checked="consentChecked"
-                  @update:checked="consentChecked = $event"
-                />
-                <Button :is-form-valid="isFormValid" button-text="Enviar" />
-              </form>
-            </div>
-          </div>
-        </div>
-      </transition>
-      <transition name="modal-transition">
-        <div v-if="showSecondFormModal && isModalOpen('news')"
-          class="modal left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
-          <div class="video-modal text-white px-5 max-w-sm absolute text-start rounded-xl">
-            <VideoSectionTwo :config="config" @closeModal="closeModal" />
-              <form @submit.prevent="submitForm">
-                <p class="font-bold text-lg py-3 text-white">
-                  {{ config.titleForms }}
-                </p>
-                <div v-for="(field, index) in config.formFields" :key="index"
-                  class="bg-black-100 flex justify-between items-center">
-                  <label :for="'field' + index" class="text-sm text-violet-300 pr-3">{{ field.label }}</label>
-                  <input :type="field.type" :id="'field' + index" :value="field.value"
-                    @input="updateFieldValue(index, $event.target.value)" required
-                    class="rounded border-none my-2 bg-violet-300 p-1 mr-10" />
-                </div>
-                <div class="flex items-center flex justify-between text-sm">
-                  <p class="text-violet-300 pr-2">{{ config.titleGender }}</p>
-                  <GenderSelect :options="config.gender" :selectedGender.sync="selectedGender" class="mr-36 pl-3" />
-                </div>
-                <CheckInput
-                  v-if="config.consentCheckbox"
-                  inputId="consentCheckbox"
-                  :label="config.shareData"
-                />
-                <CheckInput
-                  v-if="config.consentCheckbox"
-                  inputId="consentCheckbox"
-                  :label="config.acceptTerms"
-                  :checked="consentChecked"
-                  @update:checked="consentChecked = $event"
-                />
-                <Button :is-form-valid="isFormValid" button-text="Enviar" />
-              </form>
-          </div>
-        </div>
-      </transition>
-      <transition name="modal-transition">
-        <div v-if="showSecondModal"
-          class="modal left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
-          <div class="video-modal text-white px-5 max-w-sm absolute text-start rounded-xl">
-            <VideoSection :config="config" @closeSecondModal="closeSeconde" />
-              <form @submit.prevent="submitForm">
-                <p class="font-bold text-lg py-3 text-white">
-                  {{ config.titleForms }}
-                </p>
-                <div v-for="(field, index) in config.formFields" :key="index"
-                  class="bg-black-100 flex justify-between items-center">
-                  <label :for="'field' + index" class="text-sm text-violet-300 pr-3">{{ field.label }}</label>
-                  <input :type="field.type" :id="'field' + index" :value="field.value"
-                    @input="updateFieldValue(index, $event.target.value)" required
-                    class="rounded border-none my-2 bg-violet-300 p-1 mr-10" />
-                </div>
-                <div class="flex items-center flex justify-between text-sm">
-                  <p class="text-violet-300 pr-2">{{ config.titleGender }}</p>
-                  <GenderSelect :options="config.gender" :selectedGender.sync="selectedGender" class="mr-36 pl-3" />
-                </div>
-                <CheckInput
-                  v-if="config.consentCheckbox"
-                  inputId="consentCheckbox"
-                  :label="config.shareData"
-                />
-                <CheckInput
-                  v-if="config.consentCheckbox"
-                  inputId="consentCheckbox"
-                  :label="config.acceptTerms"
-                  :checked="consentChecked"
-                  @update:checked="consentChecked = $event"
-                />
-                <Button :is-form-valid="isFormValid" button-text="Enviar" />
-              </form>
-          </div>
-        </div>
-      </transition>
+  <div v-if="config">
+    <PopupHeader />
+    <PopupContainer />
+    <PopupHelp />
+    <PopupButtons :openModal="openModal" />
+    <div v-if="modalSuccess"
+      class="left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
+      <MyModal :config="config" :spiner-loading="spinerLoading" :modal-cupom="modalCupom" :cupom="cupom"
+        :cut-code="cutCode" :past-code="pastCode" @closeNow="closeNow" @copiar-numero="copiarNumero"
+        @hidden-cut="hiddenCut" />
     </div>
+    <transition name="modal-transition">
+      <div v-if="bases"
+        class="left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
+        <div class="after-modal max-w-sm absolute text-start rounded-xl">
+          <div class="py-2 px-5">
+            <PopupHeaderModal :config="config" @close-in="closeIn" v-if="true" />
+            <form @submit.prevent="submitForm">
+              <p class="font-bold text-lg py-3 text-white">
+                {{ config.titleForms }}
+              </p>
+              <div v-for="(field, index) in config.formFields" :key="index"
+                class="bg-black-100 flex justify-between items-center">
+                <label :for="'field' + index" class="text-sm text-violet-300 pr-3">{{ field.label }}</label>
+                <input :type="field.type" :id="'field' + index" :value="field.value"
+                  @input="updateFieldValue(index, $event.target.value)" required
+                  class="rounded border-none my-2 bg-violet-300 p-1 mr-10" />
+              </div>
+              <div class="flex items-center flex justify-between">
+                <p class="text-violet-300 pr-2">{{ config.titleGender }}</p>
+                <GenderSelect :options="config.gender" :selectedGender.sync="selectedGender" class="mr-36" />
+              </div>
+              <CheckInput v-if="config.consentCheckbox" inputId="consentCheckbox" :label="config.shareData" />
+              <CheckInput v-if="config.consentCheckbox" inputId="consentCheckbox" :label="config.acceptTerms"
+                :checked="consentChecked" @update:checked="consentChecked = $event" />
+              <Button :is-form-valid="isFormValid" button-text="Enviar" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <transition name="modal-transition">
+      <div v-if="showFormModal && isModalOpen('form')"
+        class="left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
+        <div class="after-modal max-w-sm absolute text-start rounded-xl">
+          <div class="py-2 px-5">
+            <PopupHeaderModalTwo :config="config" @closeModal="closeModal" />
+            <form @submit.prevent="submitForm">
+              <p class="font-bold text-lg py-3 text-white">
+                {{ config.titleForms }}
+              </p>
+              <div v-for="(field, index) in config.formFields" :key="index"
+                class="bg-black-100 flex justify-between items-center">
+                <label :for="'field' + index" class="text-sm text-violet-300 pr-3">{{ field.label }}</label>
+                <input :type="field.type" :id="'field' + index" :value="field.value"
+                  @input="updateFieldValue(index, $event.target.value)" required
+                  class="rounded border-none my-2 bg-violet-300 p-1 mr-10" />
+              </div>
+              <div class="flex items-center flex justify-between">
+                <p class="text-violet-300 pr-2">{{ config.titleGender }}</p>
+                <GenderSelect :options="config.gender" :selectedGender.sync="selectedGender" class="mr-36" />
+              </div>
+              <CheckInput v-if="config.consentCheckbox" inputId="consentCheckbox" :label="config.shareData" />
+              <CheckInput v-if="config.consentCheckbox" inputId="consentCheckbox" :label="config.acceptTerms"
+                :checked="consentChecked" @update:checked="consentChecked = $event" />
+              <Button :is-form-valid="isFormValid" button-text="Enviar" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <transition name="modal-transition">
+      <div v-if="showSecondFormModal && isModalOpen('news')"
+        class="left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
+        <div class="video-modal text-white px-5 max-w-sm absolute text-start rounded-xl">
+          <VideoSectionTwo :config="config" @closeModal="closeModal" />
+          <form @submit.prevent="submitForm">
+            <p class="font-bold text-lg py-3 text-white">
+              {{ config.titleForms }}
+            </p>
+            <div v-for="(field, index) in config.formFields" :key="index"
+              class="bg-black-100 flex justify-between items-center">
+              <label :for="'field' + index" class="text-sm text-violet-300 pr-3">{{ field.label }}</label>
+              <input :type="field.type" :id="'field' + index" :value="field.value"
+                @input="updateFieldValue(index, $event.target.value)" required
+                class="rounded border-none my-2 bg-violet-300 p-1 mr-10" />
+            </div>
+            <div class="flex items-center flex justify-between text-sm">
+              <p class="text-violet-300 pr-2">{{ config.titleGender }}</p>
+              <GenderSelect :options="config.gender" :selectedGender.sync="selectedGender" class="mr-36 pl-3" />
+            </div>
+            <CheckInput v-if="config.consentCheckbox" inputId="consentCheckbox" :label="config.shareData" />
+            <CheckInput v-if="config.consentCheckbox" inputId="consentCheckbox" :label="config.acceptTerms"
+              :checked="consentChecked" @update:checked="consentChecked = $event" />
+            <Button :is-form-valid="isFormValid" button-text="Enviar" />
+          </form>
+        </div>
+      </div>
+    </transition>
+    <transition name="modal-transition">
+      <div v-if="showSecondModal"
+        class="left-0 top-0 w-screen h-screen absolute text-center flex justify-center items-center z-50 bg-black bg-opacity-50">
+        <div class="video-modal text-white px-5 max-w-sm absolute text-start rounded-xl">
+          <VideoSection :config="config" @closeSecondModal="closeSeconde" />
+          <form @submit.prevent="submitForm">
+            <p class="font-bold text-lg py-3 text-white">
+              {{ config.titleForms }}
+            </p>
+            <div v-for="(field, index) in config.formFields" :key="index"
+              class="bg-black-100 flex justify-between items-center">
+              <label :for="'field' + index" class="text-sm text-violet-300 pr-3">{{ field.label }}</label>
+              <input :type="field.type" :id="'field' + index" :value="field.value"
+                @input="updateFieldValue(index, $event.target.value)" required
+                class="rounded border-none my-2 bg-violet-300 p-1 mr-10" />
+            </div>
+            <div class="flex items-center flex justify-between text-sm">
+              <p class="text-violet-300 pr-2">{{ config.titleGender }}</p>
+              <GenderSelect :options="config.gender" :selectedGender.sync="selectedGender" class="mr-36 pl-3" />
+            </div>
+            <CheckInput v-if="config.consentCheckbox" inputId="consentCheckbox" :label="config.shareData" />
+            <CheckInput v-if="config.consentCheckbox" inputId="consentCheckbox" :label="config.acceptTerms"
+              :checked="consentChecked" @update:checked="consentChecked = $event" />
+            <Button :is-form-valid="isFormValid" button-text="Enviar" />
+          </form>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
